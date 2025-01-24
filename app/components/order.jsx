abbb4257c2b42a57c8017c0cb50b276050ccd07b
Ahmad Azizyar <115074167+ilxli-nl@users.noreply.github.com>
2025-01-24 16:56:11 +0100

    gelukt

--- app/components/order.jsx
+++ app/components/order.jsx
@@ -2,6 +2,14 @@
 import { Suspense } from 'react'
 import { OrderBol,OrderImg } from "../actions/actions";
 import Imagebol from  '../components/image';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 
 async function Order({ordId}) {
@@ -22,6 +30,18 @@ async function Order({ordId}) {
                 <h1>test: {item.orderItemId}</h1>
                 <h1>test: {item.product.ean}</h1>
                 <Imagebol ean={item.product.ean} />
+                <Card>
+  <CardHeader>
+    <CardTitle>{item.product.title}</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
               </Suspense>
             </div>
           ))}
