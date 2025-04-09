97fad2c7096a38922037940b9ed02194537ff243
Ahmad Azizyar <115074167+ilxli-nl@users.noreply.github.com>
2025-04-09 15:26:50 +0200

    Addational search working

--- app/actions/search.js
+++ app/actions/search.js
@@ -5,8 +5,15 @@ export async function searchOrders(query) {
   // ...
   return await prisma.orders.findMany({
     where: {
-      OR: [{ orderId: { contains: query, lte: 'insensitive' } }],
+      OR: [
+        { orderId: { contains: query, lte: 'insensitive' } },
+
+        { title: { contains: query } },
+        { s_zipCode: { contains: query, lte: 'insensitive' } },
+        { s_firstName: { contains: query, lte: 'insensitive' } },
+        { s_surname: { contains: query, lte: 'insensitive' } },
+      ],
     },
-    take: 10,
+    take: 50,
   });
 }
