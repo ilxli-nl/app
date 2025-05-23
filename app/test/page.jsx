fdca7c1bc4d8e03df55aeadc3160d617d311f9ab
Ahmad Azizyar <115074167+ilxli-nl@users.noreply.github.com>
2025-05-23 13:47:57 +0200

    pagination working!

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,13 +1,17 @@
+import { auth } from '@/auth';
+import { SignInButton } from '@/components/sign-in-button';
 import  OrderList  from '@/components/OrderList';
 import Paginations from '@/components/pagination';
 
 
-function Test() {
-
+const Test = async ({ searchParams }) => {
+const session = await auth();
   const myRnId = () => parseInt(Date.now() * Math.random());
-  const page = 1
+  const page = await searchParams['page'];
   const account = 'NL'
-  return (
+    if (session?.user.name == 'ilxli-nl') {
+    return (
+ 
 
     <div className='bg-slate-300 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
         <main
@@ -24,7 +28,12 @@ function Test() {
       </div>
 
   )
+  }
+  return (
+    <div>
+      <p> You Are Not Signed In</p> <SignInButton />
+    </div>
+  );
 }
 
 export default Test
-
